cc0ef81cf0ae90ff7ad18bd2c2cef1856d452cad
Sir Nightmare <ivantochilkin@gmail.com>
2016-08-01 21:23:31 +0300

    Initial commit

---
+++ src/ru/ncedu/java/tasks/WordCounterImpl.java
@@ -0,0 +1,77 @@
+package ru.ncedu.java.tasks;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Sir Nightmare on 08/01/16.
+ */
+public class WordCounterImpl implements WordCounter
+{
+    /**
+     * Принимает текст для анализа
+     *
+     * @param text текст для анализа
+     */
+    @Override
+    public void setText(String text) {
+
+    }
+
+    /**
+     * @return текст, переданный для анализа при последнем вызове метода
+     * {@link #setText(String) setText}, или <code>null</code>,
+     * если указанный метод еще не вызывался или последний раз вызывался
+     * с параметром <code>null</code>
+     */
+    @Override
+    public String getText() {
+        return null;
+    }
+
+    /**
+     * Возвращает {@link Map}&lt;{@link String}, {@link Long}&gt;,
+     * сопоставляющую каждому слову количество его вхождений в анализируемый текст.
+     * Все возвращаемые слова должны быть приведены к нижнему регистру.
+     *
+     * @return результат подсчета количеств вхождений слов
+     * @throws IllegalStateException если не задан текст для анализа
+     *                               (метод {@link #setText(String) setText} еще не вызывался
+     *                               или последний раз вызывался с параметром <code>null</code>)
+     */
+    @Override
+    public Map<String, Long> getWordCounts() throws IllegalStateException {
+        return null;
+    }
+
+    /**
+     * Возвращает список из {@link /Entry Map.Entry}&lt;{@link String}, {@link Long}&gt;,
+     * сопоставляющий каждому слову количество его вхождений в анализируемый текст
+     * и упорядоченный в прядке убывания количества вхождений слова.<br/>
+     * Слова с одинаковым количеством вхождений упорядочиваются в алфавитном порядке (без учета регистра!).<br/>
+     * Все возвращаемые слова должны быть приведены к нижнему регистру.
+     *
+     * @return упорядоченный результат подсчета количеств вхождений слов
+     * @throws IllegalStateException если не задан текст для анализа
+     *                               (метод {@link #setText(String) setText} еще не вызывался
+     *                               или последний раз вызывался с параметром <code>null</code>)
+     */
+    @Override
+    public List<Map.Entry<String, Long>> getWordCountsSorted() throws IllegalStateException {
+        return null;
+    }
+
+    /**
+     * Упорядочивает результат подсчета количеств вхождений слов в порядке убывания количества вхождений.<br/>
+     * Слова с одинаковым количеством вхождений упорядочиваются в алфавитном порядке (без учета регистра!).<br/>
+     * Реализация этого метода не является обязательной, но если он будет реализован, то реализация
+     * метода {@link #getWordCountsSorted()} будет тривиальной.
+     *
+     * @param orig неупорядоченный результат подсчета
+     * @return упорядоченный результат подсчета или <code>null</code>, если <code>orig == null</code>
+     */
+    @Override
+    public List<Map.Entry<String, Long>> sortWordCounts(Map<String, Long> orig) {
+        return null;
+    }
+}
